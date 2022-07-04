d06a135bb163a24ac18b1c885f6abeeff9f5f4be
“hu-snail” <1217437592@qq.com>
2022-07-04 23:03:33 +0800

    🍃fix: Adjust directory structure

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 // https://github.com/hmsk/vite-plugin-markdown
 import mdPlugin, { Mode } from 'vite-plugin-markdown'
-
-import createRenderer from './build/plugin/md-render'
 import markdownIt from 'markdown-it'
 import {
   containerPlugin,
   highlight,
   highlightLinePlugin,
   headingPlugin
-} from './build/plugin/markdown'
+} from './src/plugin/markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,9 +22,6 @@ export default defineConfig({
     vue(),
     mdPlugin({
       mode: [Mode.HTML, Mode.TOC, Mode.VUE],
-      markdown: (content) => {
-        return createRenderer(content)
-      },
       markdownIt: markdownIt({ html: true, highlight })
         .use(containerPlugin)
         .use(highlightLinePlugin)
