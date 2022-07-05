f606cf973abdb39e9bd445c6513c70e19c3b60d3
“hu-snail” <1217437592@qq.com>
2022-07-05 23:03:26 +0800

    feat: add favicon

--- src/layout/site.vue
+++ src/layout/site.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script lang="ts" setup>
+import SnailNavbar from './components/navbar/index.vue'
+</script>
 <template>
   <div>
+    <SnailNavbar/>
     <div class="text-5xl font-extrabold ...">
       <span
         class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"
